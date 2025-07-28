5e626709404ace75eda9d53cf84ed0fd201713f2
mateusz.byczkowski <mateusz.byczkowski@covantis.io>
2025-07-28 22:13:02 +0200

    debug message transfer

--- backend/src/modules/summaries/summaries.module.ts
+++ backend/src/modules/summaries/summaries.module.ts
@@ -1,8 +1,14 @@
 import { Module } from '@nestjs/common';
+import { BullModule } from '@nestjs/bullmq';
 import { SummariesController } from './summaries.controller';
 import { SummariesService } from './summaries.service';
 
 @Module({
+  imports: [
+    BullModule.registerQueue({
+      name: 'summarization',
+    }),
+  ],
   controllers: [SummariesController],
   providers: [SummariesService],
   exports: [SummariesService],
